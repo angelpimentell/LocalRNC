9b69965fc9d3f1a2f97f26ef0b17c620eb2a04a3
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-02-21 10:17:12 -0400

    Add company model

---
+++ LocalRNC/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalRNC.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public required string RNC { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public required DateTime Created_at { get; set; }
+    }
+}
