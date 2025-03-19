bf908d1c641288011b186f92c5b43387b9347449
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-03-19 05:44:25 -0400

    Add authorize for Company endpoint

--- LocalRNC/Controllers/CompanyController.cs
+++ LocalRNC/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 ﻿using LocalRNC.Data;
 using LocalRNC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace LocalRNC.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CompanyController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
